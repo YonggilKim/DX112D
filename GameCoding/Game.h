cc29ed26374eb383ab8212b30d744c4e67488a93
yonggil kim <gtmddls41@gmail.com>
2023-02-17 21:42:28 +0900

    삼각형그리기
    1

--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 class Game
 {
@@ -26,6 +27,14 @@ private:
 	uint32 _height = 0;
 
 private:
+	void CreateGeometry();
+	void CreateInputLayout();
+	void CreateVS();
+	void CreatePS();
+	void LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob);
+
+
+private:
 	// Device & SwapChain
 	ComPtr<ID3D11Device> _device = nullptr;
 	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
@@ -38,5 +47,18 @@ private:
 	D3D11_VIEWPORT _viewport = { 0 };
 	float _clearColor[4] = { 0.f, 0.f, 0.f, 0.f };
 
+private:
+	//geometry
+	vector<Vertex> _vertices;
+	ComPtr<ID3D11Buffer> _vertexBuffer = nullptr;
+	ComPtr<ID3D11InputLayout> _inputLayout = nullptr;
+
+	// VS
+	ComPtr<ID3D11VertexShader> _vertexShader = nullptr;
+	ComPtr<ID3DBlob> _vsBlob = nullptr;
+
+	// PS
+	ComPtr<ID3D11PixelShader> _pixelShader = nullptr;
+	ComPtr<ID3DBlob> _psBlob = nullptr;
 };
 
